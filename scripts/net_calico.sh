03f9b3bec735e2f209f59e6e0759181ff7b0cfc6
Jeff Gao <zengfa.gao@charter.com>
2018-06-06 13:30:41 -0600

    Get rid of root, use /Users/jeffgao

--- scripts/net_calico.sh
+++ scripts/net_calico.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-kubeadm init --pod-network-cidr=192.168.0.0/16 > /root/kube-configuration/kube-init.log
+kubeadm init --pod-network-cidr=192.168.0.0/16 > $HOME/kube-configuration/kube-init.log
 cp -f /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f /root/kube-configuration/networks/rbac.yaml
-kubectl apply -f /root/kube-configuration/networks/calico.yaml
-kubectl create -f /root/kube-configuration/networks/kubernetes-dashboard.yaml
+kubectl apply -f $HOME/kube-configuration/networks/rbac.yaml
+kubectl apply -f $HOME/kube-configuration/networks/calico.yaml
+kubectl create -f $HOME/kube-configuration/networks/kubernetes-dashboard.yaml
--- scripts/net_canal.sh
+++ scripts/net_canal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-kubeadm init --pod-network-cidr=10.244.0.0/16 > /root/kube-configuration/kube-init.log
+kubeadm init --pod-network-cidr=10.244.0.0/16 > $HOME/kube-configuration/kube-init.log
 cp -f /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f /root/kube-configuration/networks/canal.yaml
-kubectl create -f /root/kube-configuration/networks/kubernetes-dashboard.yaml
+kubectl apply -f $HOME/kube-configuration/networks/canal.yaml
+kubectl create -f $HOME/kube-configuration/networks/kubernetes-dashboard.yaml
