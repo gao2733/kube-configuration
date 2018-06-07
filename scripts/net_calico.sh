5d1e2f7d8233e230caeb55016bb13fe2de06c8cb
Jeff Gao <zengfa.gao@charter.com>
2018-06-07 09:01:38 -0600

    Fix dashboard yaml file

--- scripts/net_calico.sh
+++ scripts/net_calico.sh
@@ -2,6 +2,6 @@
 kubeadm init --pod-network-cidr=192.168.0.0/16 > $HOME/kube-configuration/kube-init.log
 cp -f /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f $HOME/kube-configuration/networks/rbac.yaml
+kubectl apply -f $HOME/kube-configuration/networks/rbac-kdd.yaml
 kubectl apply -f $HOME/kube-configuration/networks/calico.yaml
-kubectl create -f $HOME/kube-configuration/networks/kubernetes-dashboard.yaml
+kubectl create -f $HOME/kube-configuration/networks/kubernets-dashboard.yaml
--- scripts/net_romana.sh
+++ scripts/net_romana.sh
@@ -3,4 +3,4 @@ kubeadm init --pod-network-cidr=10.244.0.0/16 > $HOME/kube-configuration/kube-in
 cp -f /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl apply -f $HOME/kube-configuration/networks/romana-kubeadm.yml
-kubectl create -f $HOME/kube-configuration/networks/kubernetes-dashboard.yaml
+kubectl create -f $HOME/kube-configuration/networks/kubernets-dashboard.yaml
